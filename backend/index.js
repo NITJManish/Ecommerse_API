fbe6de24899238860390aff64381618860d9d05e
neera <kumarmanish150799@gmail.com>
2024-06-07 15:33:37 +0530

    backend api of ecommerse

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const dotenv=require('dotenv').config();
+const dotenv=require('dotenv');
 const port=process.env.port || 8000;
 const express=require('express');
 const app=express();
@@ -8,13 +8,13 @@ const multer=require('multer');
 const cors=require('cors');
 const path=require('path');
 const { type } = require('os');
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
 
 //Database connection with mongodb
-mongoose.connect("mongodb+srv://kumarmanish150799:DHNSiiJWg9j6i6MZ@cluster0.osgiteu.mongodb.net/ecommerse-data?retryWrites=true&w=majority&appName=Cluster0");
-
+mongoose.connect(process.env.MongoDB_URL);
 
 //API creation
 app.get("/",(req,res)=>{
@@ -165,7 +165,7 @@ app.post("/signup",async (req,res)=>{
             id:user.id
         }
     }
-    const token=jwt.sign(data,'secret_key');
+    const token=jwt.sign(data,SecuretKey);
     res.json({success:true,token}); 
 })
 
@@ -180,7 +180,7 @@ app.post("/login",async (req,res)=>{
                     id:user.id
                 }
             }
-            const token=jwt.sign(data,'secret_key');
+            const token=jwt.sign(data,SecuretKey);
             res.json({success:true,token});
         }else{
             res.json({success:false,errors:"Wrong password"});
@@ -213,7 +213,7 @@ const fetchUser= async (req,res,next)=>{
     res.status(401).send({errors:"Please authenticate using valid token"})
   }else{
     try{
-        const data=jwt.verify(token,'secret_key');
+        const data=jwt.verify(token,SecuretKey);
         req.user=data.user;
         next();
     }catch(error){
